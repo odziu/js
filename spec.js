670b45242ffc9dbe30288c67e43997ffdd02d958
odziu <dzyubak12@gmail.com>
2019-01-28 14:29:57 +0200

    new test format. test runs correctly

--- spec.js
+++ spec.js
@@ -3,6 +3,6 @@ describe('Enter GURU99 Name', () => {
         await browser.get('https://angularjs.org');
         await element(by.model('yourName')).sendKeys('GURU99');
         let guru= element(by.xpath('html/body/div[2]/div[1]/div[2]/div[2]/div/h1'));
-        expect(guru.getText()).toEqual('Hello change GURU99!');
+        expect(await guru.getText()).toEqual('Hello ccc GURU99!');
     });
 });
